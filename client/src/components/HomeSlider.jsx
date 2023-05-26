5b4e85dbe611821793eb6863f5387157ea611363
Ranjan <just.think2019@gmail.com>
2023-05-26 10:30:18 +0530

    Backend

---
+++ client/src/components/HomeSlider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const HomeSlider = () => {
+    return (
+        <div className=''>HomeSlider</div>
+    )
+}
+
+export default HomeSlider
